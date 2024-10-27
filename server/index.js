f22d51e35b0c8cc6f678e4ae4259be685a0f2225
Atharv Grover <100594021+NoScopeJedi83@users.noreply.github.com>
2024-10-27 13:15:21 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,24 +1,29 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
-const dotenv = require("dotenv").config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js");
+const listingRoutes = require("./routes/listing.js");
+const bookingRoutes = require("./routes/booking.js");
+const userRoutes = require("./routes/user.js");
 
-const authRoutes = require("./routes/auth.js")
-const listingRoutes = require("./routes/listing.js")
-const bookingRoutes = require("./routes/booking.js")
-const userRoutes = require("./routes/user.js")
+// Set up CORS to allow requests from your front-end URL
+app.use(cors({
+  origin: "https://localhost:3000/", // Replace with your actual front-end URL
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
 /* ROUTES */
-app.use("/auth", authRoutes)
-app.use("/properties", listingRoutes)
-app.use("/bookings", bookingRoutes)
-app.use("/users", userRoutes)
+app.use("/auth", authRoutes);
+app.use("/properties", listingRoutes);
+app.use("/bookings", bookingRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = 3001;
